94d02713fd96c23ff75d50051da7f60635997f57
raipratik <pratikkumar399@gmail.com>
2023-08-22 23:24:24 +0530

    revising bst

---
+++ Trees/bstt.cpp
@@ -0,0 +1,59 @@
+// ॐ नमः शिवाय
+#include <bits/stdc++.h>
+using namespace std;
+
+/* जय भवानी । हर हर महादेव। */
+
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+Node *insertIntoBst(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        root = new Node(data);
+        return root;
+    }
+
+    if (data <= root->data)
+    {
+        root->left = insertIntoBst(root->left, data);
+    }
+    else
+    {
+        root->right = insertIntoBst(root->right, data);
+    }
+    return root;
+}
+
+void takeInput(Node *&root)
+{
+
+    int data;
+    cin >> data;
+
+    while (data != -1)
+    {
+        root = insertIntoBst(root, data);
+        cin >> data;
+    }
+}
+
+int main()
+{
+
+    Node *root = NULL;
+    takeInput(root);
+    display(root);
+    return 0;
+}
