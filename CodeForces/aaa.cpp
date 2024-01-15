08460c5e091061c30688f49fcf2e13ab3c7e2591
raipratik <pratikkumar399@gmail.com>
2024-01-15 21:16:24 +0530

    b done

--- CodeForces/aaa.cpp
+++ CodeForces/aaa.cpp
@@ -11,18 +11,27 @@ int main()
     while (t--)
     {
         // coordinates of square as input
-        int k = 4;
-        int maxi = INT_MIN, mini = INT_MAX; // Initialize maxi and mini with extreme values
-        while (k--)
+        int n;
+        cin >> n;
+        string str, stt;
+        cin >> str >> stt;
+        // now we need to find how many pairs are there of 01 and 10
+        int i = 0, j = 0;
+        int count1 = 0, count2 = 0;
+        while (i < str.size())
         {
-            int x, y;
-            cin >> x >> y;
-
-            maxi = max(maxi, x);
-            mini = min(mini, x);
+            if (str[i] == '1' && stt[j] == '0')
+            {
+                count1++;
+            }
+            else if (str[i] == '0' && stt[j] == '1')
+            {
+                count2++;
+            }
+            i++;
+            j++;
         }
-        int ans = maxi - mini;
-        cout << ans * ans << endl;
+        cout << max(count1, count2) << endl;
     }
 
     return 0;
