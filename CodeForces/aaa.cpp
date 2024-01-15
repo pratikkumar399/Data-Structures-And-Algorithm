cbe0f30ff31ef5e1fd609256d30f16ae98ce77b7
raipratik <pratikkumar399@gmail.com>
2024-01-15 23:50:57 +0530

    d done

--- CodeForces/aaa.cpp
+++ CodeForces/aaa.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 /* जय भवानी । हर हर महादेव। */
+
 int main()
 {
     int t;
@@ -10,31 +11,57 @@ int main()
 
     while (t--)
     {
+        int n, m;
+        cin >> n >> m;
 
-        long long n, f, a, b;
-        cin >> n >> f >> a >> b;
-        vector<int> arr(n);
-        for (auto &it : arr)
+        vector<int> vasya(n), petya(m);
+        for (auto &it : vasya)
             cin >> it;
-        bool flag = false;
-        long long prev = 0;
-        for (int i = 0; i < n; i++)
+        for (auto &it : petya)
+            cin >> it;
+
+        sort(vasya.begin(), vasya.end(), greater<int>());
+        sort(petya.begin(), petya.end());
+
+        long long a = 0, b = n - 1;
+        long long c = 0, d = m - 1;
+        long long sum = 0;
+        while (a <= b)
         {
-            long long chargeCount = (arr[i] - prev) * a;
-            prev = arr[i];
-            long long mini = min(chargeCount, b);
-            f -= mini;
-            if (f <= 0)
+            int temp1 = abs(vasya[a] - petya[c]);
+            int temp2 = abs(vasya[b] - petya[d]);
+            if (temp1 > temp2)
             {
-                flag = true;
-                break;
+                sum += temp1;
+                a++;
+                c++;
+            }
+            else if (temp1 < temp2)
+            {
+                sum += temp2;
+                b--;
+                d--;
+            }
+            else
+            {
+                sum += temp2;
+                if (vasya[a] > petya[a])
+                {
+
+                    b--;
+                    d--;
+                }
+                else
+                {
+                    a++;
+                    c++;
+                }
             }
         }
-        if (flag == true)
-            cout << "No" << endl;
-        else
-            cout << "Yes" << endl;
+        cout << sum << endl;
     }
 
     return 0;
 }
+
+// 6 4 1 2  , 1 2 3 3 5 7  ->   ->
